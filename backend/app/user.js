06fc4cc4a6f5cdca4d05b83d644a0c3133552b9b
Jean Maidana <jean0king@gmail.com>
2021-11-27 11:11:45 -0300

    Implements hashedPassword

--- backend/app/user.js
+++ backend/app/user.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 let userSchema = new mongoose.Schema({
   name:String,
@@ -11,20 +10,6 @@ let userSchema = new mongoose.Schema({
     type:mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
-  }
-});
-
-userSchema.pre("save", (next) => {
-  if(this.isNew || this.isModified('password')){
-    bcrypt.hash(this.password, 10,
-      (err, hashedPassword) => {
-        if(err)
-        next(err)
-        else {
-          this.password = hashedPassword;
-          next();
-        }
-      })
   }
 });
 
