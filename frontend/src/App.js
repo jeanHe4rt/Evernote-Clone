b051ce8408ff9d742ce2c4cd0152ff3241be21d3
jeanat2 <jean0king@gmail.com>
2021-12-12 13:56:15 -0300

    ADD header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
+import Header from "./components/header";
+import "./App.scss";
 
 const App = () => {
-  return (
-    <p> Meu site.com</p>
-  )
-}
+  return <Header />;
+};
 export default App;
