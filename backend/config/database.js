fcae34d8578798a7354da38fd387d8ef313fee56
jeanat2 <jean0king@gmail.com>
2021-12-17 15:39:50 -0300

    First deploy

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose') ;
 mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost:27017/evernoteDB', {
+require('dotenv').config();
+// mongodb+srv://jean:Ellen@2021@cluster0.o55rz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const MONGO_URL = process.env.MONGO_URL;
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log('connection successful'))
   .catch((err) => console.error(err))
+
