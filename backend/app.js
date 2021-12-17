ee3fbd83aa26e38722ef962fa460e4d77b081eb5
jeanat2 <jean0king@gmail.com>
2021-12-17 16:46:50 -0300

    First Deploy 2

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,8 @@ app.use('/notes', notesRouter);
 app.listen(3001, function() {
   console.log('CORS-enabled web server listening on port 3001');
 });
-app.listen(process.env.PORT || 3001, function(){
+
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
