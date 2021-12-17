984bc627b36bc81551b75c0ae69c84e75bdbe371
jeanat2 <jean0king@gmail.com>
2021-12-17 16:34:04 -0300

    First Deploy 2

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose') ;
 mongoose.Promise = global.Promise;
 require('dotenv').config();
 // mongodb+srv://jean:Ellen@2021@cluster0.o55rz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+require('dotenv').config();
 const MONGO_URL = process.env.MONGO_URL;
-mongoose.connect(MONGO_URL, {
-  useNewUrlParser: true,
+mongoose.connect(MONGO_URL, { useNewUrlParser: true, 
   useUnifiedTopology: true,
-}).then(() => console.log('connection successful'))
-  .catch((err) => console.error(err))
-
+  useCreateIndex: true
+}).then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
